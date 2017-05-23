4f85b54f6ff7f25dc05f3e81a25e98c1cc0f54f2
root <root@csakis.localdomain>
2017-05-23 12:37:52 +0000

    updates to cms

--- cms/accommodations.js
+++ cms/accommodations.js
@@ -3,7 +3,7 @@
 	greeting: 'Welcome!',
 	subtitle: 'We love Jesus!',
 	section_one_title: 'Szálláshelyek',
-	section_one_body_text: 'List with links here',
+	section_one_body_text: '<a href="https://www.kezdi.info/sporthostel" target="_blank">Hostel Sport / Sportszálló</a><br/>\n<a href="http://www.gaboraron.ro/oldalak/bentlakasgaleria.html" target="_blank">Gábor Áron Műszaki Oktatási Központ bentlakása</a><br/>\n<a href="http://fortyogo.weebly.com/" target="_blank">Fortyogó Vendégház</a><br/>\n<a href="http://www.enikovendeghaz.ro/" target="_blank">Enikő Vendégház</a><br/>\n<a href="https://www.facebook.com/rebapanzio/" target="_blank">Réba Panzió</a><br/>\n<a href="http://www.szekelyfoldiinfo.ro/Menu/Szallashelyek/csernaton-szallas-hanna_panzio-csernaton-hotelek-villak-panziok.html" target="_blank">Hanna Panzió</a><br/>',
 	$section_one_body_text_type: 'textarea',
 	section_one_image: 'http://res.cloudinary.com/csakis-fesztival/image/upload/v1492542146/Csakis-3-48_hfjzdn.jpg',
 	$section_one_image_type: 'image',
