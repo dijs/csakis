24d48b37b44225ac53240e1b91fa88e53b524911
root <root@csakis.localdomain>
2018-06-30 14:13:51 +0000

    Remote changes

--- cms/index.js
+++ cms/index.js
@@ -4,19 +4,19 @@
 	subtitle: 'We love Jesus!',
 	section_one_title: 'CSAKIS idén is!',
 	section_one_body_text: 'III. CSAKIS Családos Keresztyén  Fesztivál Kézdivásárhelyen\n<br/><br/> \n\nÖrömmel értesítjük, hogy Székelyföldön, Kézdivásárhelyen, 2018 . július 6-7. között harmadik alkalommal szervezzük meg a Családos Keresztyén Fesztivált.<br/> E sportcsarnokban és környékén megtartandó eseménnyel egy nyári pihenési és töltekezési lehetőséget szeretnénk biztosítani a családoknak.<br/> Fesztiválunk programja és légköre kimondottan keresztyén lesz, ahol áhítatokkal, előadásokkal, lelkigondozói- és fórumbeszélgetésekkel kívánjuk erősíteni házasságainkat Isten segedelmével. <br/>Emellett sok játék, gyerekfoglalkozás, ífjúsági program, vetélkedő, lovaglás, sport és koncertek várnak kedves vendégeinkre. <br/>A fesztivál fő előadói Dr. Horváth Levente és Mária. Esténként zenei élményt szolgáltat, dicsőítést vezet a Kovászna Belváros  ífjúsági  Zenekara és Csiszér László.<br/>\nVárunk szeretettel minden családot.<br/>\nA szervező csapat',
-	section_two_title: 'Kézdivásárhelyi Sportcsarnok és környéke',
-	section_two_body_text: 'itt vagyunk. google map link itt van.',
-	section_three_title: 'Magunkról',
-	section_three_body_text: 'A Kézdivásárhelyi Családos Keresztény Fesztivállal egy nyári pihenési és töltekezési lehetőséget szeretnénk biztosítani a családoknak. Mivel a családok nyaranta rengeteg kikapcsolódási lehetőség közül válogathatnak, ezért a mi fesztiválunk programja és légköre kimondottan keresztény lesz. Kirándulási, kikapcsolódási, töltekezési és sportolási lehetőségek mellett a házasságot érintő gondolatébresztőkön, előadásokon, életút beszélgetéseken vehetnek részt a fesztiválra látogatók. Természetesen a fesztiválok elvárásainak eleget téve koncertekre is sor kerül, de dicsőítésre, példaértékű családokkal fórumbeszélgetésekre, élményszínházra és egyéb alternatív programokra is sor kerül a rendezvényen. Mivel a család egészének kínálunk programot, ezért a gyerekek, ifjak részére külön programot is szervezünk, ahol ki-ki a korosztályának megfelelő tevékenységekbe kapcsolódhat be.',
-	section_three_map_link: 'http://google.com',
+	section_two_title: 'Magunkról',
+	section_two_body_text: 'A Kézdivásárhelyi Családos Keresztény Fesztivállal egy nyári pihenési és töltekezési lehetőséget szeretnénk biztosítani a családoknak. Mivel a családok nyaranta rengeteg kikapcsolódási lehetőség közül válogathatnak, ezért a mi fesztiválunk programja és légköre kimondottan keresztyén lesz. Kirándulási, kikapcsolódási, töltekezési és sportolási lehetőségek mellett a házasságot érintő gondolatébresztőkön, előadásokon, életút beszélgetéseken vehetnek részt a fesztiválra látogatók. Természetesen a fesztiválok elvárásainak eleget téve koncertekre is sor kerül, de dicsőítésre, példaértékű családokkal fórumbeszélgetésekre  és egyéb alternatív programokra is sor kerül a rendezvényen. Mivel a család egészének kínálunk programot, ezért a gyerekek, ifjak részére külön programot is szervezünk, ahol ki-ki a korosztályának megfelelő tevékenységekbe kapcsolódhat be',
+	section_three_title: 'Kézdivásárhelyi Sportcsarnok és környéke',
+	section_three_body_text: 'Itt vagyunk!',
+	section_three_map_link: 'https://www.google.com/maps/place/K%C3%A9zdiv%C3%A1s%C3%A1rhelyi+Sportcsarnok/@45.9830147,26.0388814,13z/data=!4m8!1m2!2m1!1sSportcsarnok!3m4!1s0x40b4eaf070619b95:0x8e297843028b9919!8m2!3d45.999736!4d26.1248429',
 	$section_one_body_text_type: 'textarea',
-	section_one_image: 'https://scontent.fsbz1-2.fna.fbcdn.net/v/t31.0-8/13920430_1754485428163162_5558796261105781116_o.jpg?oh=e7b4339b9e7171aaa97aeed4fb11aa5b&oe=599575A5',
+	section_one_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/32169061_2065881520356883_3432458028111101952_n.jpg?_nc_cat=0&oh=f1f2c0472734b7cb398cd136e1c48c1b&oe=5BA29146',
 	$section_one_image_type: 'image',
-	section_two_image: 'https://scontent.fsbz1-2.fna.fbcdn.net/v/t31.0-8/14068400_1756894361255602_6747156726755247737_o.jpg?oh=58fb3ef56d9dbf53946b39b7d5e961e5&oe=59628B05',
+	section_two_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/32169444_2065881340356901_5057292478864949248_n.jpg?_nc_cat=0&oh=e7f0ec82981179b93db3484573fcf751&oe=5BA83D58',
 	$section_two_image_type: 'image',
-	section_three_image: 'https://scontent.fsbz1-2.fna.fbcdn.net/v/t1.0-9/19665467_1916720148606355_5686054569823914336_n.jpg?oh=6064947403f374bc43d9c15ccdf2e691&oe=5A0AF3EE',
+	section_three_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/32089883_2065881223690246_7221662392084594688_n.jpg?_nc_cat=0&oh=514506a78b8006ee412daca6cb576477&oe=5BB0402C',
 	$section_three_image_type: 'image',
-	section_four_image: 'https://scontent.fsbz1-2.fna.fbcdn.net/v/t1.0-9/13939386_1756874724590899_2696227255947323066_n.jpg?oh=dc07009d73ba07e2cef8f958a15e39d1&oe=59CB6083',
+	section_four_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/36365577_2099110350367333_5134398459066974208_n.png?_nc_cat=0&oh=2550e416abb4df97dbcdd82fc820ed0e&oe=5BA807FC',
 	$section_four_image_type: 'image',
 	$banner_image_type: 'image',
 	banner_image: 'http://res.cloudinary.com/csakis-fesztival/image/upload/v1520444389/2018-CSAKISposterHorNEW_lqsags.jpg',
@@ -26,17 +26,27 @@
 		{
 			$gallery_image_type: 'image',
 			gallery_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t31.0-8/20369627_1929072440704459_8224573323586722876_o.jpg?_nc_cat=0&oh=5ab9f2bc96a2324e72ad699cf004f7ee&oe=5B5F4D3A',
-			title: 'Park'
+			title: '2017'
 		},
 		{
 			$gallery_image_type: 'image',
 			gallery_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/19748667_1918253578453012_5251767305712287294_n.jpg?_nc_cat=0&oh=80b35e1d1126ad7206ae471de288f626&oe=5B54EFEA',
-			title: 'Ball'
+			title: '2017'
 		},
 		{
 			$gallery_image_type: 'image',
 			gallery_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/19905146_1918267268451643_2310872946314239739_n.jpg?_nc_cat=0&oh=3f6c704bf86c9407563e6ef131be33ec&oe=5B67731C',
-			title: 'Stage'
+			title: '2017'
+		},
+		{
+			$gallery_image_type: 'image',
+			gallery_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/32191308_2065881750356860_6229179309215449088_o.jpg?_nc_cat=0&oh=804f258e7696f641e7c8732f39118508&oe=5B85D6CA',
+			title: '2017'
+		},
+		{
+			$gallery_image_type: 'image',
+			gallery_image: 'https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/32130281_2065881740356861_4612865824468762624_o.jpg?_nc_cat=0&oh=2361e493b80242459a9c2ec1b1027b51&oe=5B9C8F3C',
+			title: '2017'
 		}
 	]
 }
